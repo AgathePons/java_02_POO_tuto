8e7ffff9d7180654b00af1161cada82a72b7960a
AgathePons <ponsagathe37@gmail.com>
2022-09-30 16:51:31 +0200

    Classes + constructor + new instances

--- src/tuto/poo/Classes.java
+++ src/tuto/poo/Classes.java
@@ -3,8 +3,37 @@ package tuto.poo;
 public class Classes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		SpecialAbility evilLaugh = new SpecialAbility("Evil Laugh", "Produce an Evil laugh");
+		System.out.println("Ability: " + evilLaugh.name);
+
+		Monster boogieMan = new Monster("Boogie Man", 2, evilLaugh);
+		System.out.println(boogieMan.name + " is scary because of his " + boogieMan.ability.name);
+	}
+
+	static class Monster {
+		String name;
+		int eyesNumber;
+		boolean isScary = true;
+		SpecialAbility ability;
+
+		// constructor
+		public Monster(String name, int eyesNumber, SpecialAbility ability) {
+			this.name = name;
+			this.eyesNumber = eyesNumber;
+			this.ability = ability;
+		}
+	}
+
+	static class SpecialAbility {
+		String name;
+		String effect;
+
+		// constructor
+		public SpecialAbility(String name, String effect) {
+			this.name = name;
+			this.effect = effect;
+		}
 	}
 
 }
