f8df721065aae0d58d6f91db6f3ab912a44060c8
AgathePons <ponsagathe37@gmail.com>
2022-10-02 15:07:37 +0200

    add legacy class

--- README.md
+++ README.md
@@ -50,3 +50,74 @@ Monster boogieMan = new Monster("Boogie Man", 2, evilLaugh);
 System.out.println(boogieMan.name + " is scary because of his " + boogieMan.ability.name);
 ```
 
+## Legacy class
+
+A class can inherit an other class with the key word `extends`.
+
+```java
+static class SuperHero {
+    void hello() {
+        System.out.println("Hello, I am a superhero!");
+    }
+}
+
+static class SuperVilain extends SuperHero {}
+
+static class SuperNice extends SuperHero {}
+```
+
+In this example, `SuperVilain` and `SuperNice` have acces to the `SuperHero` method `hello()`.
+
+```java
+SuperVilain theJoker = new SuperVilain();
+theJoker.hello();
+SuperNice batman = new SuperNice();
+batman.hello();
+```
+
+A child class can inherit only one parent class, but the parent class can be the child class of another class.
+
+An object from a **legacy class** is an object of the first class. In the example, `theJoker` and `batman` are 
+both `SuperHero`.
+
+To initialize the inherited attributes, we have to use the keyword `super`:
+
+```java
+public class FigureGeometrique {
+    private int posX;
+    private int posY;
+
+    public void moveTo(int newPosX, int newPosY) {
+        this.posX = newPosX;
+        this.posY = newPosY;
+    }
+    // constructor
+    FigureGeometrique(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+}
+
+public class Carre extends FigureGeometrique {
+    private long cote;
+
+    public long getCote() {
+        return cote;
+    }
+
+    public long getPerimetre() {
+        return 4 * cote;
+    }
+
+    Carre(long cote, int posX, int posY) {
+        // call the constructor of the parent class
+        super(posX, posY);
+        this.cote = cote;
+    }
+}
+```
+
+## Polymorphism
+
+
+---------------------------
--- src/tuto/poo/Classes.java
+++ src/tuto/poo/Classes.java
@@ -3,21 +3,36 @@ package tuto.poo;
 public class Classes {
 
 	public static void main(String[] args) {
-
+		// classes
 		SpecialAbility evilLaugh = new SpecialAbility("Evil Laugh", "Produce an Evil laugh");
 		System.out.println("Ability: " + evilLaugh.name);
 
 		Monster boogieMan = new Monster("Boogie Man", 2, evilLaugh);
 		System.out.println(boogieMan.name + " is scary because of his " + boogieMan.ability.name);
+
+		// legacy
+		SuperVilain theJoker = new SuperVilain(127, "The Joker", "Harley Quinn", 3, false);
+		theJoker.sayHello();
+		theJoker.sayDeclaration();
+		System.out.println("He killed " + theJoker.innocentPeopleKilledNumber + " innocent people...");
+		SuperNice batman = new SuperNice(76, "Batman", "Robin", 4, false);
+		batman.sayHello();
+		batman.sayDeclaration();
+		System.out.println("He saved " + batman.innocentPeopleSaveedNumber + " innocent people...");
+		System.out.println("Batman's sidekick is " + batman.sideKick + ".");
+		batman.sideKick = "Catwoman";
+		System.out.println("Batman's new sidekick is " + batman.sideKick + ".");
 	}
 
+	// classes
 	static class Monster {
 		String name;
 		int eyesNumber;
 		boolean isScary = true;
 		SpecialAbility ability;
 
-		// constructor - isScary has a default value, so it is not mandatory in the constructor
+		// constructor - isScary has a default value, so it is not mandatory in the
+		// constructor
 		public Monster(String name, int eyesNumber, SpecialAbility ability) {
 			this.name = name;
 			this.eyesNumber = eyesNumber;
@@ -36,4 +51,89 @@ public class Classes {
 		}
 	}
 
+	// legacy
+	static class SuperHero {
+		private final String name;
+		public String sideKick;
+		public int alliesNumber;
+		public boolean hasSuperPower;
+
+		void sayHello() {
+			System.out.println("Hello, my name is " + name + " and I am a superhero!");
+		}
+
+		SuperHero(String name, String sideKick, int alliesNumber, boolean hasSuperPower) {
+			this.name = name;
+			this.sideKick = sideKick;
+			this.alliesNumber = alliesNumber;
+			this.hasSuperPower = hasSuperPower;
+		}
+	}
+
+	static class SuperVilain extends SuperHero {
+		public int innocentPeopleKilledNumber;
+
+		void sayDeclaration() {
+			System.out.println("I will kill innocent people!");
+		}
+
+		SuperVilain(int innocentPeopleKilledNumber, String name, String sideKick, int alliesNumber,
+				boolean hasSuperPower) {
+			super(name, sideKick, alliesNumber, hasSuperPower);
+			this.innocentPeopleKilledNumber = innocentPeopleKilledNumber;
+		}
+
+	}
+
+	static class SuperNice extends SuperHero {
+		public int innocentPeopleSaveedNumber;
+
+		void sayDeclaration() {
+			System.out.println("I will save the world from bad guys!");
+		}
+
+		SuperNice(int innocentPeopleSaveedNumber, String name, String sideKick, int alliesNumber,
+				boolean hasSuperPower) {
+			super(name, sideKick, alliesNumber, hasSuperPower);
+			this.innocentPeopleSaveedNumber = innocentPeopleSaveedNumber;
+
+		}
+	}
+
+	// other example
+	public class FigureGeometrique {
+		private int posX;
+		private int posY;
+
+		public void moveTo(int newPosX, int newPosY) {
+			this.posX = newPosX;
+			this.posY = newPosY;
+		}
+
+		FigureGeometrique(int posX, int posY) {
+			this.posX = posX;
+			this.posY = posY;
+		}
+	}
+
+	public class Carre extends FigureGeometrique {
+		private long cote;
+
+		public long getCote() {
+			return cote;
+		}
+
+		public long getPerimetre() {
+			return 4 * cote;
+		}
+
+		Carre(long cote, int posX, int posY) {
+			// call the constructor of the parent class
+			super(posX, posY);
+			this.cote = cote;
+		}
+	}
+
 }
+
+// ------------
